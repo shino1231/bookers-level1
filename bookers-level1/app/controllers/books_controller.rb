12c11d66accb43e9b010cb6bddd7ff81cc5f92e1
EC2 Default User <fuku.trfcmg@gmail.com>
2020-10-13 08:03:30 +0000

    [Add] 投稿一覧機能を追加2

--- bookers-level1/app/controllers/books_controller.rb
+++ bookers-level1/app/controllers/books_controller.rb
@@ -1,26 +1,23 @@
 class BooksController < ApplicationController
-  def index
-  end
-
-  def show
-  end
 
   def new
     @book = Book.new
+    @books = Book.all
   end
-  
+
   def create
     book = Book.new(book_params)
     book.save
     redirect_to books_path
   end
 
-  def edit
+  def index
   end
-  
+
   private
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
