6e29617024c82d6075c8dd59d492d49e484bf0ab
EC2 Default User <fuku.trfcmg@gmail.com>
2020-10-13 09:46:20 +0000

    [Add] 編集機能の追加

--- bookers-level1/app/controllers/books_controller.rb
+++ bookers-level1/app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-
+  
   def new
     @book = Book.new
     @books = Book.all
@@ -8,14 +8,28 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to books_path
+    redirect_to books_path(book.id)
   end
 
   def index
   end
-
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
+  
+  def show
+    @book = Book.find(params[:id])  
+  end
+  
   private
-
+  
   def book_params
     params.require(:book).permit(:title, :body)
   end
